5af816ec157937bcbaba7405c808570a17e52122
rainyday0613 <rainyday0613@gmail.com>
2020-10-20 10:19:50 +0900

    front structure

--- Cafe/frontend/src/main.js
+++ Cafe/frontend/src/main.js
@@ -1,14 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import VueComp from '@vue/composition-api'
+Vue.config.productionTip = false
+import router from './routes';
+import store from './store';
 
-Vue.config.productionTip = false;
+Vue.use(VueComp)
 
 new Vue({
   router,
   store,
-  vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
--- Cafe/frontend/vue.config.js
+++ Cafe/frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+	publicPath: process.env.NODE_ENV === 'production' ? '/vue-pro-sidebar/' : '/',
+	chainWebpack: config => {
+	//	process.env.NODE_ENV === 'production' ? config.externals(['@vue/composition-api']):{};
+	},
+	css: {
+		extract: false,
+	},
 };
