86f0ade244d1c30adbb1ee8f62b79ccc79214291
bitai <an0160@naver.com>
2020-10-21 12:29:19 +0900

    opence test file

---
+++ proj/python/opencvtest/Test/People-Counting-in-Real-Time-master/mylib/config.py
@@ -0,0 +1,22 @@
+#===============================================================================
+""" Optional features config. """
+#===============================================================================
+# Enter mail below to receive real-time email alerts
+# e.g., 'email@gmail.com'
+MAIL = ''
+# Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
+url = ('http://192.168.0.63:8080/video')
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = False
+# Threading ON/OFF
+Thread = False
+
+# Simple log to log the counting data
+Log = False
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+# Auto stop the software after certain a time/hours
+Timer = False
+#===============================================================================
+#===============================================================================
