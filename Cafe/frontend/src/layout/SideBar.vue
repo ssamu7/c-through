69d8b7c80a27383f1800e31bab27b197856aedab
ssamu7 <ssamu7@naver.com>
2020-10-23 10:20:12 +0900

    sidebar modified

--- Cafe/frontend/src/layout/SideBar.vue
+++ Cafe/frontend/src/layout/SideBar.vue
@@ -19,7 +19,8 @@
         <div
             class="vps-sidebar-user-status"
             :style="{'--status-color':true?'#06EF61':'#FB0508'}"
-        >Online</div>
+        >Online
+        </div>
       </div>
     </div>
     <div class="vps-sidebar-search">
@@ -45,7 +46,7 @@
               height="16px"
               width="16px"
           />
-          <div class="vps-sidebar-menu-item-content-label" >
+          <div class="vps-sidebar-menu-item-content-label">
             <router-link to="/Menu">
               {{item.label}}
             </router-link>
@@ -96,6 +97,7 @@ import Icon from "../components/icons";
 import Avatar from "../components/Avatar";
 import Badge from "../components/Badge";
 import EventBus from "../utils/EventBus.js";
+
 export default {
   name: "side-bar",
   data() {
@@ -226,10 +228,13 @@ export default {
   transition: all 1s;
   max-height: 100%;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+{
   transform: translateY(-100%);
   height: 0;
 }
+
 .flip-list-move {
   transition: all 1s;
 }
